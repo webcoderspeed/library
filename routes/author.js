1462ca17bfcf9fb613311664cfa3780aa80eecc1
SpeedSharma <speedsniper919@gmail.com>
2021-03-30 20:05:40 +0530

    Author Config

--- routes/author.js
+++ routes/author.js
@@ -6,7 +6,7 @@ const Author = require('../models/author')
 router.get('/', async (req, res) => {
 
   let searchOptions = {}
-  if(req.query.name !== null && req.query.name !== ''){
+  if(req.query.name != null && req.query.name !== ''){
     searchOptions.name = new RegExp(req.query.name, 'i')
   }
 
